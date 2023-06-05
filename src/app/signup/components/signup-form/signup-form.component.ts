5b186115ded4418fcf4bb75306c4e12d50b19081
Carl Giesberts <carl@front-crafter.nl>
2023-06-05 15:02:53 +0200

    Added reactive forms

--- src/app/signup/components/signup-form/signup-form.component.ts
+++ src/app/signup/components/signup-form/signup-form.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'signup-form',
   templateUrl: './signup-form.component.html',
   styleUrls: ['./signup-form.component.less'],
 })
-export class SignupForm {}
+export class SignupForm {
+  firstName = new FormControl();
+  lastName = new FormControl();
+  email = new FormControl();
+}
--- src/app/signup/signup.module.ts
+++ src/app/signup/signup.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignupForm } from './components/signup-form/signup-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   declarations: [SignupForm],
   providers: [SignupForm],
   exports: [SignupForm],
